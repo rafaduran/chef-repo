6fa816ad02abbf9918594c15deb093857cd38026
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2012-08-11 21:25:20 +0200

    Adding/ignoring more configuration files
    * Adding Cheffile.lock as librarian docs suggests
    * Adding my knife.rb configuration file
    * Ignoring some unwanted files

---
+++ .chef/knife.rb
@@ -0,0 +1,17 @@
+require 'librarian/chef/integration/knife'
+
+current_dir =           File.dirname(__FILE__)
+user =                  ENV['OPSCODE_USER'] || ENV['USER']
+log_level               :info
+log_location            STDOUT
+node_name               user
+client_key              "#{ENV['HOME']}/.chef/#{user}.pem"
+validation_client_name  "#{ENV['ORGNAME']}-validator"
+validation_key          "#{ENV['HOME']}/.chef/#{ENV['ORGNAME']}-validator.pem"
+chef_server_url         "https://api.opscode.com/organizations/#{ENV['ORGNAME']}"
+cache_type              'BasicFile'
+cache_options           :path => "#{ENV['HOME']}/.chef/checksums"
+cookbook_path           Librarian::Chef.install_path
+cookbook_copyright      ENV['COOKBOOK_COPYRIGHT'] || `git config --get user.name`.strip
+cookbook_license        "apachev2"
+cookbook_email          ENV['COOKBOOK_EMAIL'] || `git config --get user.email`.strip
