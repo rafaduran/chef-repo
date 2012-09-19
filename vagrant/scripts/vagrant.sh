8ef1fb96ada7d0c7457b5fa7ac14f2d5202eec1e
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2012-09-19 23:04:26 +0200

    Forcing Chef 0.10.10.
    Chef 10.14.2 breaks Mnitest handler cookbook, so forcing 0.10.10 instead
    of updating to latest version wherever Chef needs to be updated.

--- vagrant/scripts/vagrant.sh
+++ vagrant/scripts/vagrant.sh
@@ -6,5 +6,6 @@ VERSION=`chef-solo --version | awk -F. '{print $3}'`
 
 # We need at least 0.10.10
 if [[ "$VERSION" -lt 10 ]]; then
-    gem update chef
+    # Since Debian doesn't work with latest Chef, forcing 0.10.10
+    gem install chef --version 0.10.10 --no-ri --no-rdoc
 fi
